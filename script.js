d151b9330cfd213378b716c498bc4d2e40c02380
zarifatb-15 <zarifatb@code.edu.az>
2026-02-26 23:24:56 +0400

    added 2.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // 1
-let number =prompt("Eded daxil edin");
-let n= parseInt(number);
-if(n %3==0 && n %7==0)
-console.log("Eded 3 ve 7-e bolunur");
-else
-console.log("Eded 3 ve 7-e bolunmur");
+// let number =prompt("Eded daxil edin");
+// let n= parseInt(number);
+// if(n %3==0 && n %7==0)
+// console.log("Eded 3 ve 7-e bolunur");
+// else
+// console.log("Eded 3 ve 7-e bolunmur");
+
+// 2
+
+let count=0;
+let n=0;
+let m=10;
+for(let i=n+1; i<m; i++){
+     if(i%2!==0)
+     count++;
+}
+console.log(count);
