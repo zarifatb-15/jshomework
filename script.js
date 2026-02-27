1f5a6f9148f1a31fb14c81dc079360c4d1c210fb
zarifatb-15 <zarifatb@code.edu.az>
2026-02-28 00:19:57 +0400

    added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 1
+// 1.ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
 // let number =prompt("Eded daxil edin");
 // let n= parseInt(number);
 // if(n %3==0 && n %7==0)
@@ -6,7 +6,7 @@
 // else
 // console.log("Eded 3 ve 7-e bolunmur");
 
-// 2
+// 2 Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin sayini tapin.
 
 // let count = 0;
 // let n = 0;
@@ -16,8 +16,17 @@
 // }
 // console.log(count);
 
-// 3
+// 3 Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
 
+// let sum=0;
+// let n = 0;
+// let m = 10;
+
+// for (let i = n + 1; i < m; i++) {
+//   if (i % 2 !== 0) 
+//     sum+=i;
+// }
+// console.log(sum);
 
 
 
