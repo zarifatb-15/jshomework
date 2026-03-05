520326c39c48671c87ec9a8bc1f5ac65faa71774
zarifatb-15 <zarifatb@code.edu.az>
2026-03-05 22:18:13 +0400

    added

--- script.js
+++ script.js
@@ -84,17 +84,19 @@
 // let result = sumEvenNumbers(data);
 // console.log(`Cut ededlerin cemi ${result}`);
  
-  
-
-
-
-
-
-
-
 
 
 // 6)Her hansisa number elementleri olan arayin icindeki cut ededlerin kvadratlarinin cemini hesablayan bir function yazin.
+
+let arr=[1,2,3,4,5,6];
+let sum=0;
+
+for(let i=0; i<arr.length; i++){
+    if(arr[i]%2===0){
+        sum+=arr[i]**2;
+    }
+}
+console.log(`Cut ededlerin kvadratlarinin cemi ${sum}`);    
 
 
 
